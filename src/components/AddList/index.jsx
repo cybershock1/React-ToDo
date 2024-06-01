9ae6339d831a7a3681d3ab3543673f95f34b4eb2
cybershock1 <pavicaes@gmail.com>
2024-06-01 21:17:13 +0300

    I completed the 3rd video lesson.

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -6,9 +6,26 @@ import closeSvg from "../../assets/img/close.svg";
 
 import "./AddButtonList.scss";
 
-const AddList = ({ colors }) => {
+const AddList = ({ colors, onAdd }) => {
   const [visiblePopup, setVisiblePopup] = useState(false);
   const [selectedColor, selectColor] = useState(colors[0].id);
+  const [inputValue, setInputValue] = useState("");
+
+  const onClose = () => {
+    setVisiblePopup(false);
+    setInputValue('');
+    selectColor(colors[0].id);
+  }
+
+  const AddList = () => {
+    if (!inputValue) {
+      alert("Введите название списка");
+      return;
+    }
+    const color = colors.filter(c => c.id === selectedColor)[0].name;
+    onAdd({ id: Math.random(), name: inputValue, color });
+    onClose();
+  };
 
   return (
     <div className="add-list">
@@ -48,8 +65,19 @@ const AddList = ({ colors }) => {
 
       {visiblePopup && (
         <div className="add-list__popup">
-          <img onClick={() => setVisiblePopup(false)} src={closeSvg} alt="Close button" className="add-list__popup-close-btn" />
-          <input className="field" type="text" placeholder="Название списка" />
+          <img
+            onClick={onClose}
+            src={closeSvg}
+            alt="Close button"
+            className="add-list__popup-close-btn"
+          />
+          <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="field"
+            type="text"
+            placeholder="Название списка"
+          />
           <div className="add-list__popup-colors">
             {colors.map((color) => (
               <Badge
@@ -60,7 +88,9 @@ const AddList = ({ colors }) => {
               />
             ))}
           </div>
-          <button className="button">Добавить</button>
+          <button onClick={AddList} className="button">
+            Добавить
+          </button>
         </div>
       )}
     </div>
